a10ec9454345d6c3b02962e68365c21dc2eaef15
daevon johnson <daevontjohnson01@gmail.com>
2021-12-10 15:54:26 -0600

    delete buttom

--- movie-project.js
+++ movie-project.js
@@ -30,17 +30,22 @@ const rendermovies=(movies)=> {
         '<div class="director"> Directed by ' + movie.director + '</div>'+
         '<div class="rating"> Rating: ' + movie.rating + '</div>'+
         '<div class="actors"> Starring: ' + movie.actors + '</div>'+
-        '<div class="plot">'+ movie.plot + '</div> +
+        '<div class="plot">'+ movie.plot + '</div>' +
         // EDIT & DELETE BUTTONS
         // Rendered with the particular movie ID, this allows targeting of the class "edit-btn" or "delete-btn"
         // for simplified event function, while still allowing *this* particular movie to be targeted to PATCH or DELETE
-        '<button class="edit-btn" value="' + movie.id + '">Edit</button>' +
-        '<button class="delete-btn" value="' + movie.id + '">Delete</button>' +
+        '<button class="edit-btn"  data-id="' + movie.id + '">Edit</button>' +
+        '<button class="delete-btn" data-id="' + movie.id + '">Delete</button>' +
         // '<div class="title">'+ movie.  '</div>+
         '</div>'
     }
     // Set the HTML of the target to the given string of elements & data.
     $('#movies').html(moviesHTML);
+    $('.delete-btn').click(function (){
+        let buttonvalue = $(this).data("id");
+        console.log(buttonvalue)
+        alert("hello")
+    })
 }
 
 // FORM SUBMISSION: ADD FUNCTION
@@ -63,4 +68,20 @@ let createMovie = (movie) => {
     // after posting, getMovies is called again to update the list
     fetch(URL, options).then(resp => resp.json()).then(getmovies()).catch(err => console.error(err));
 }
+let deleteMovie = (movieid) => {
+    let options = {
+        method: 'DELETE',
+        headers: {'Content-Type': 'application/json'},
+    }
+   return  fetch(`${URL}/${movieid}`, options).then(resp => resp.json()).then(getmovies()).catch(err => console.error(err));
+}
+$('.delete-btn').click(function (){
+    let buttonvalue = $(this).data("id");
+    console.log(buttonvalue)
+alert("hello")
+})
 
+// deleteMovie(5);
+//     $(document).ready(function(){
+// }
+//     $("#edit-btn").prop("value", "Input New Text");
